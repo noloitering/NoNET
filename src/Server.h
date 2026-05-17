37e98574f386dc15a8ceeb0e0ab0763e8becd23c
No Loitering Studios <neverloiter@gmail.com>
2026-05-17 00:04:49 -0230

    Initial Commit

---
+++ src/Server.h
@@ -0,0 +1,435 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+// name conflicts with raylib
+#define CloseWindow CloseWinWindow
+#define Rectangle WinRectangle
+#define ShowCursor ShowWinCursor
+#define LoadImage LoadWinImage
+#define DrawText DrawWinText
+#define DrawTextEx DrawWinTextEx
+#define PlaySound PlayWinSound
+#include <enet/enet.h>
+#undef CloseWindow
+#undef Rectangle
+#undef ShowCursor
+#undef LoadImage
+#undef DrawText
+#undef DrawTextEx
+#undef PlaySound
+
+#include <vector>
+
+namespace NoNET
+{
+	//TODO: I just threw static here to shut the compiler up. Find the actual correct design decision to handle this (may involve cpp, maybe a config file?)
+	static const size_t DEFAULTCHANNELS = 2;
+	static const size_t MAXCONNECTIONS = 32;
+	static const size_t MAXCLIENTNAME =  32;
+	static const enet_uint16 DEFAULTPORT = 1234;
+	static const ENetAddress DEFAULTADDRESS = (ENetAddress){ENET_HOST_ANY, NoNET::DEFAULTPORT};
+	enum State {OFFLINE, ONLINE};
+	
+	struct Bandwidth
+	{
+		enet_uint32 in = 0; // unlimited incoming bandwidth
+		enet_uint32 out = 0; // unlimited outgoing bandwidth
+	};
+	
+	// struct ClientInfo
+	// {
+	// public:
+		// size_t id;
+		// char user[MAXCLIENTNAME];
+		// ENetAddress addr;
+		// ENetPeer* peer;
+	// }
+		
+	class Host
+	{
+	protected:
+		ENetHost* host = nullptr;
+		ENetAddress* addr = NULL;
+		ENetEvent event;
+		Bandwidth bandwidth;
+		State state = NoNET::OFFLINE;
+		size_t maxPeers;
+		size_t channels;
+		virtual void onConnect(ENetEvent& connectEvent) = 0;
+		virtual void onReceive(ENetEvent& receiveEvent) = 0;
+		virtual void onDisconnect(ENetEvent& discconectEvent) = 0;
+	public:
+		Host(NoNET::Bandwidth maxBandwidth, size_t maxConnections=MAXCONNECTIONS, size_t maxChannels=DEFAULTCHANNELS)
+			: bandwidth(maxBandwidth), maxPeers(maxConnections), channels(maxChannels) {}
+		Host(enet_uint32 maxin = 0, enet_uint32 maxout = 0, size_t maxConnections=MAXCONNECTIONS, size_t maxChannels=DEFAULTCHANNELS)
+			: maxPeers(maxConnections), channels(maxChannels) {bandwidth.in = maxin; bandwidth.out = maxout;}
+		size_t getMaxPeers()
+		{
+			
+			return maxPeers;
+		}
+		size_t getChannels()
+		{
+			
+			return channels;
+		}
+		Bandwidth getBandwidth()
+		{
+			
+			return bandwidth;
+		}
+		State getState()
+		{
+			
+			return state;
+		}
+		ENetEvent getEvent()
+		{
+			
+			return event;
+		}
+		ENetAddress* getAddress()
+		{
+			
+			return addr;
+		}
+		const char* getIP()
+		{
+			size_t strLength = 17;
+			char ip[strLength];
+			if ( addr )
+			{
+				if ( enet_address_get_host_ip(addr, ip, strLength) == 0 )
+				{
+				
+					return TextFormat(ip);
+				}
+				else
+				{
+				
+					return "";
+				}
+			}
+		}
+		const char* getHostname()
+		{
+			size_t strLength = 17;
+			char ip[strLength];
+			if ( addr )
+			{
+				if ( enet_address_get_host(addr, ip, strLength) == 0 )
+				{
+				
+					return TextFormat(ip);
+				}
+				else
+				{
+				
+					return "";
+				}
+			}
+		}
+		// TODO: return event, or event type, or an int based on success?
+		ENetEvent poll(enet_uint32 sleep)
+		{
+			if ( enet_host_service(host, &event, sleep) > 0 )
+			{
+//				std::cout << "received event" << std::endl;
+				switch(event.type)
+				{
+					case ENET_EVENT_TYPE_CONNECT:
+					{
+						onConnect(event);
+						
+						break;
+					}
+			
+					case ENET_EVENT_TYPE_RECEIVE:
+					{	
+						onReceive(event);
+						enet_packet_destroy(event.packet);
+						
+						break;
+					}
+			
+					case ENET_EVENT_TYPE_DISCONNECT:
+					{	
+						onDisconnect(event);
+						event.peer -> data = NULL;
+						
+						break;
+					}
+				}
+			}
+			
+			return event;
+		}
+		friend int startup(const ENetAddress* addr, NoNET::Host& host, bool force);
+		friend void shutdown(NoNET::Host& host, bool force);
+		
+		virtual ~Host()
+		{
+			if ( state == NoNET::ONLINE )
+			{
+				enet_host_destroy(host);
+			}
+		}
+	};
+	
+//	wrapper for enet_host_create
+	inline int startup(const ENetAddress* addr, NoNET::Host& host, bool force=false)
+	{
+		if ( host.state == NoNET::OFFLINE || force )
+		{
+			host.host = enet_host_create(addr, host.maxPeers, host.channels, host.bandwidth.in, host.bandwidth.out);
+			if ( host.host == nullptr || host.host == NULL )
+			{
+				std::cerr << "An error occurred while trying to create an ENet client host." << std::endl;
+				
+				return 1;
+			}
+			else
+			{
+				std::cout << "host initialized properly" << std::endl;
+				host.state = NoNET::ONLINE;
+				
+				return 0;
+			}
+		}
+		
+		return 0;
+	}
+	
+	inline void shutdown(NoNET::Host& host, bool force=false)
+	{
+		if ( host.state == NoNET::ONLINE || force )
+		{
+			enet_host_destroy(host.host);
+			host.state = NoNET::OFFLINE;
+		}
+	}
+	
+	class Client : public Host
+	{
+	private:
+//		size_t id;
+		char name[MAXCLIENTNAME];
+		ENetPeer* peer = nullptr;
+	public:
+		Client(const char* clientname, NoNET::Bandwidth max, size_t maxConnections=MAXCONNECTIONS, size_t maxChannels=DEFAULTCHANNELS)
+			: Host(max, maxConnections, maxChannels) {name[0] = '\0'; strncat(name, clientname, MAXCLIENTNAME - 1);}
+		Client(const char* clientname, enet_uint32 maxin = 0, enet_uint32 maxout = 0, size_t maxConnections=MAXCONNECTIONS, size_t maxChannels=DEFAULTCHANNELS)
+			: Host(maxin, maxout, maxConnections, maxChannels) {name[0] = '\0'; strncat(name, clientname, MAXCLIENTNAME - 1);}
+		const char* getName()
+		{
+			
+			return name;
+		}
+		
+		int connect(ENetAddress address, enet_uint32 timeout=5000)
+		{
+			char IPBuffer[17];
+			enet_address_get_host(&address, IPBuffer, 17);
+			std::cout << "attempting connection to: " << IPBuffer << " on port: " << address.port << std::endl;
+			peer = enet_host_connect(host, &address, channels, 0); // TODO: replace 0 with client name
+			if ( peer == NULL || peer == nullptr )
+			{
+				
+				return 1;
+			}
+			else
+			{
+				ENetEvent event = poll(timeout);
+				if ( event.type == ENET_EVENT_TYPE_CONNECT )
+				{
+					
+					return 0;
+				}
+				else
+				{
+					enet_peer_reset(peer);
+					
+					return 1;
+				}
+				
+				// ENetEvent connectEvent;
+				// if ( enet_host_service(host, &connectEvent, timeout) > 0 && event.type == ENET_EVENT_TYPE_CONNECT)
+				// {
+					
+					// return 0;
+				// }
+				// else
+				// {
+					// enet_peer_reset(peer);
+					
+					// return 1;
+				// }
+			}
+		}
+		int connect(const char* ip, enet_uint16 port, enet_uint32 timeout=5000)
+		{
+			ENetAddress address;
+			enet_address_set_host(&address, ip);
+			
+			return connect(address, timeout);
+		}
+		
+		int send(enet_uint8 channel, ENetPacket* payload, bool force=false)
+		{
+			int res = 1;
+			if ( peer )
+			{
+				res = enet_peer_send(peer, channel, payload);
+				if ( force )
+				{
+					enet_host_flush(host);
+				}
+			}
+			
+			return res;
+		}
+		int send(enet_uint8 channel, const char* payload, enet_uint32 flags=0, bool force=false)
+		{
+			ENetPacket* packet = enet_packet_create(payload, strlen(payload) + 1, flags);
+			
+			return send(channel, packet, force);
+		}
+		
+		int disconnect(enet_uint32 timeout=3000, bool force=false)
+		{
+			if ( force )
+			{
+				enet_peer_reset(peer);
+				
+				return 0;
+			}
+			else if ( peer )
+			{
+				enet_peer_disconnect(peer, 0);
+				ENetEvent event = poll(timeout);
+				if ( event.type == ENET_EVENT_TYPE_DISCONNECT )
+				{
+					
+					return 0;
+				}
+			}
+			
+			return 1;
+		}
+	};
+	
+	// class ClientManager
+	// {
+	// private:
+		// std::vector< ClientInfo > clients;
+// //		std::vector< ClientInfo > toAdd;
+		// size_t total = 0;
+	// public:
+		// ClientManager() {}
+		// ClientManager(std::vector< ClientInfo > list)
+			// : clients(list) {}
+		// size_t addClient(ClientInfo client)
+		// {
+			// clients.push_back(client);
+			
+			// return total++;
+		// }
+		// size_t getTotal()
+		// {
+			
+			// return 
+		// }
+	// };
+	
+	class Server : public NoNET::Host
+	{
+	private:
+		
+	public:
+		Server(NoNET::Bandwidth maxBandwidth, size_t maxConnections=MAXCONNECTIONS, size_t maxChannels=DEFAULTCHANNELS)
+			: Host(maxBandwidth, maxConnections, maxChannels) 
+		{
+			addr = (ENetAddress*) malloc(sizeof(ENetAddress));
+			addr->host = DEFAULTADDRESS.host;
+			addr->port = DEFAULTADDRESS.port;
+		}
+		Server(enet_uint32 maxin = 0, enet_uint32 maxout = 0, size_t maxConnections=MAXCONNECTIONS, size_t maxChannels=DEFAULTCHANNELS)
+			: Host(maxin, maxout, maxConnections, maxChannels) 
+		{
+			addr = (ENetAddress*) malloc(sizeof(ENetAddress));
+			addr->host = DEFAULTADDRESS.host;
+			addr->port = DEFAULTADDRESS.port;
+		}
+		
+		virtual ~Server()
+		{
+			free(addr);
+		}
+		
+		ENetPeer* getPeers()
+		{
+			
+			return host->peers;
+		}
+		
+		ENetPeer getPeer(size_t index)
+		{
+			
+			return host->peers[index];
+		}
+		
+		size_t getConnectedPeers()
+		{
+			
+			return host->connectedPeers;
+		}
+		
+		int send(ENetPeer* peer, enet_uint8 channel, ENetPacket* payload, bool force=false)
+		{
+			int res = enet_peer_send(peer, channel, payload);
+			if ( force )
+			{
+				enet_host_flush(host);
+			}
+			
+			return res;
+		}
+		int send(ENetPeer* peer, enet_uint8 channel, const char* payload, enet_uint8 flags=0, bool force=false)
+		{
+			ENetPacket* packet = enet_packet_create(payload, strlen(payload) + 1, flags);
+			
+			return send(peer, channel, packet, force);
+		}
+		
+		void broadcast(enet_uint8 channel, ENetPacket* payload)
+		{
+			enet_host_broadcast(host, channel, payload);
+		}
+		void broadcast(enet_uint8 channel, const char* payload, enet_uint8 flags=0)
+		{
+			ENetPacket* packet = enet_packet_create(payload, strlen(payload) + 1, flags);
+			enet_host_broadcast(host, 0, packet);
+		}
+		
+		int disconnect(ENetPeer* peer, enet_uint32 timeout=3000, bool force=false)
+		{
+			if ( force )
+			{
+				enet_peer_reset(peer);
+				
+				return 0;
+			}
+			enet_peer_disconnect(peer, 0);
+			ENetEvent event = poll(timeout);
+			if ( event.type == ENET_EVENT_TYPE_DISCONNECT )
+			{
+					
+				return 0;
+			}
+			
+			return 1;
+		}
+	};
+}
+
+#endif
